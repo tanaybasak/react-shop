4ae3c4579c7d4149bdbaefb4031bbe53a4c71905
tanaybasak <tanoybasak@gmail.com>
2022-12-07 08:44:54 +0530

    changes

--- src/common/apollo-client.js
+++ src/common/apollo-client.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-	uri: `${process.env.REACTFURNI_BACKEND}`,
+	uri: 'https://reactfurnibackend.onrender.com/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
